b38acce70ae0f44b717a9134549726e8019b1657
Viktor Alexander Hartung <viktor@hartrusion.com>
2026-03-26 19:33:57 +0100

    Use buffer for prerendering for smoother UI performance

--- src/com/hartrusion/rbmksim/gui/elements/SelsynGauge.java
+++ src/com/hartrusion/rbmksim/gui/elements/SelsynGauge.java
@@ -25,6 +25,7 @@ import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.beans.BeanProperty;
 import java.util.ArrayList;
 import java.util.List;
@@ -77,6 +78,12 @@ public class SelsynGauge extends javax.swing.JComponent {
 
     private final List<Shape> majorTickLines = new ArrayList<>();
     private final List<Shape> minorTickLines = new ArrayList<>();
+
+    /**
+     * Contains the one-time rendered part with all the dials and so on, will
+     * only be drawn once or refreshed on size change.
+     */
+    private BufferedImage gaugeBack;
 
     /**
      * Creates new component SelsynGauge
@@ -164,12 +171,31 @@ public class SelsynGauge extends javax.swing.JComponent {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D g2 = (Graphics2D) g;
+        Ellipse2D.Float circle;
 
-        // Only do a calculation run on size change.
-        if (oldWidth != getWidth() || oldHeight != getHeight()) {
+        // Force the use of antialiasing to not look like 1997
+        Object prevHint = g2.getRenderingHint(RenderingHints.KEY_ANTIALIASING);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // The gauges background is heavy to calculate and it is kind of static 
+        // so we only calculate it once if necessary. This improves the overal
+        // performance of the view significantly on low end hardware. It will 
+        // result in a slow delay on first rendering but that's fine.
+        if (oldWidth != getWidth() || oldHeight != getHeight()
+                || gaugeBack == null) {
             oldWidth = getWidth();
             oldHeight = getHeight();
+
+            // generate or overwrite the background image on request.
+            // Note that we use g2b instead of g2!
+            gaugeBack = new BufferedImage(oldWidth, oldHeight,
+                    BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2b = gaugeBack.createGraphics();
+            g2b.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+
             size = Math.min(oldWidth, oldHeight);
             outerRingRadius = 0.45F * (float) size;
             outerTickRadius = 0.4F * (float) size;
@@ -185,49 +211,50 @@ public class SelsynGauge extends javax.swing.JComponent {
             // only once and just draw the shapes stored in those lists
             majorTickLines.clear();
             for (int idx = 0; idx < majorTicks.length; idx++) {
-                majorTickLines.add(getLine(majorTicksInnerRadius, 
+                majorTickLines.add(getLine(majorTicksInnerRadius,
                         outerTickRadius,
                         getPhi((double) majorTicks[idx])));
             }
             minorTickLines.clear();
             for (int idx = 0; idx < minorTicks.length; idx++) {
-                minorTickLines.add(getLine(minorTicksInnerRadius, 
+                minorTickLines.add(getLine(minorTicksInnerRadius,
                         outerTickRadius,
                         getPhi((double) minorTicks[idx])));
             }
+
+            // Draw background
+            g2b.setColor(Color.WHITE);
+            circle = new Ellipse2D.Float(
+                    halfSize - outerRingRadius,
+                    halfSize - outerRingRadius,
+                    2.0F * outerRingRadius, 2.0F * outerRingRadius);
+            g2b.fill(circle);
+            // Draw the outer ring using foreground color.
+            g2b.setColor(getForeground());
+            g2b.draw(circle);
+            // draw major tick lines
+            g2b.setColor(Color.BLACK);
+            for (Shape s : majorTickLines) {
+                g2b.draw(s);
+            }
+            // Draw minor tick lines
+            for (Shape s : minorTickLines) {
+                g2b.draw(s);
+            }
+            // draw the inner cirle:
+            circle = new Ellipse2D.Float(
+                    halfSize - innerCircleRadius,
+                    halfSize - innerCircleRadius,
+                    2.0F * innerCircleRadius, 2.0F * innerCircleRadius);
+            g2b.fill(circle);
         }
         
-        // Force the use of antialiasing to not look like 1997
-        Object prevHint = g2d.getRenderingHint(RenderingHints.KEY_ANTIALIASING);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        // Draw background
-        Ellipse2D.Float circle;
-        g2d.setColor(Color.WHITE);
-        circle = new Ellipse2D.Float(
-                halfSize - outerRingRadius,
-                halfSize - outerRingRadius,
-                2.0F * outerRingRadius, 2.0F * outerRingRadius);
-        g2d.fill(circle);
-        // Draw the outer ring using foreground color.
-        g2d.setColor(getForeground());
-        g2d.draw(circle);
-        // draw major tick lines
-        g2d.setColor(Color.BLACK);
-        for (Shape s : majorTickLines) {
-            g2d.draw(s);
-        }
-        // Draw minor tick lines
-        for (Shape s : minorTickLines) {
-            g2d.draw(s);
-        }
-        // draw the inner cirle:
-        circle = new Ellipse2D.Float(
-                halfSize - innerCircleRadius,
-                halfSize - innerCircleRadius,
-                2.0F * innerCircleRadius, 2.0F * innerCircleRadius);
-        g2d.fill(circle);
+        // Draw image from buffer, either it is still present or it was 
+        // just generated.
+        g2.drawImage(gaugeBack, 0, 0, null);
+
         // generate the pointer as a triangle
+        g2.setColor(Color.BLACK);
         Path2D.Float triangle = new Path2D.Float();
         // path through 3 points:
         triangle.moveTo(getXCoordinate(pointerWidth, phiValue - PI_HALF),
@@ -237,25 +264,25 @@ public class SelsynGauge extends javax.swing.JComponent {
         triangle.lineTo(getXCoordinate(pointerWidth, phiValue + PI_HALF),
                 getYCoordinate(pointerWidth, phiValue + PI_HALF));
         triangle.closePath();
-        g2d.fill(triangle);
+        g2.fill(triangle);
         // Draw the litte inidcator light on the bottom
         if (showIndicator) {
             if (indicatorStatus) {
-                g2d.setColor(Color.GREEN);
+                g2.setColor(Color.GREEN);
             } else {
-                g2d.setColor(Color.GRAY);
+                g2.setColor(Color.GRAY);
             }
             circle = new Ellipse2D.Float(
                     halfSize - indicatorSize * 0.5F - 1.0F,
                     size - indicatorSize - 1.0F, // 1 px up, clips...
                     indicatorSize, indicatorSize);
-            g2d.fill(circle);
+            g2.fill(circle);
             // Draw the outer ring in black
-            g2d.setColor(Color.BLACK);
-            g2d.draw(circle);
+            g2.setColor(Color.BLACK);
+            g2.draw(circle);
         }
         // Reset the antialiasing to its previous value
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, prevHint);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, prevHint);
     }
 
     /**
